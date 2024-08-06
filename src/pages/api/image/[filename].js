14e3e31e27e8b712c898ba83d2ccf3e00a4a6ead
John Binze Escol <escoljohnbinze@gmail.com>
2024-08-07 01:59:01 +0800

    Added delete function and added mime-types for content header.

--- src/pages/api/image/[filename].js
+++ src/pages/api/image/[filename].js
@@ -1,22 +1,57 @@
 import fs from 'fs'
 import path from 'path'
+import mime from 'mime-types'
 
-export default function handler(req, res) {
+const handler = async (req, res) => {
   const { filename } = req.query
 
   // Set the directory where your images are stored
   const filePath = path.resolve('./uploads', filename)
+  try {
+    if (req.method === 'DELETE') {
+      if (!filename) {
+        return res.status(400).json({ error: 'File Name is required' })
+      }
 
-  // Read the image file from the file system
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.status(404).json({ error: 'Image not found' })
+      try {
+        fs.unlink(filePath, err => {
+          if (err) {
+            console.error(err)
 
-      return
+            return res.status(500).json({ error: 'Failed to delete image' })
+          }
+        })
+
+        res.status(200).json({ message: 'Image deleted successfully' })
+      } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Failed to delete image' })
+      }
+    } else if (req.method === 'GET') {
+      const mimeType = mime.lookup(filePath) || 'application/octet-stream'
+
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          console.error(err)
+
+          return res.status(404).json({ error: 'Image not found' })
+        }
+
+        res.setHeader('Content-Type', mimeType)
+        res.send(data)
+      })
+    } else {
+      res.status(405).json({ error: 'Method Not Allowed' })
     }
+  } catch (error) {
+    console.log(error)
 
-    // Set the content type to the image type (e.g., image/jpeg, image/png)
-    res.setHeader('Content-Type', 'image') // Modify as needed
-    res.send(data)
-  })
+    if (error) {
+      return res.status(500).json({ message: error.message })
+    } else {
+      return res.status(500).json({ message: 'Something went wrong' })
+    }
+  }
 }
+
+export default handler
