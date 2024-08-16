3bbe8315bc0a331c7cf9ca2620f5801ec038ab5f
John Binze Escol <escoljohnbinze@gmail.com>
2024-08-16 18:19:15 +0800

    Added bao role.

--- src/layouts/components/acl/getHomeRoute.js
+++ src/layouts/components/acl/getHomeRoute.js
@@ -2,9 +2,10 @@
  *  Set Home URL based on User Roles
  */
 const getHomeRoute = role => {
-  if (role === 'security_guard') return '/user/logs'
+  if (role === 'security_guard') return '/home'
   else if (role === 'super_admin') return '/home'
   else if (role === 'admin') return '/home'
+  else if (role === 'bao') return '/home'
   else return '/login'
 }
 
