dfd555554eb213a24d1cab24941f0962a8af9611
John Binze Escol <escoljohnbinze@gmail.com>
2024-09-26 17:15:01 +0800

    In progress fix for home page.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -11,18 +11,58 @@ import UserDetails from 'src/views/pages/home/UserDetails'
 import axios from 'axios'
 
 const Home = () => {
-  const [userCount, setUserCount] = useState(0)
+  const [studentCount, setStudentCount] = useState(0)
+  const [staffCount, setStaffCount] = useState(0)
+  const [guardianCount, setGuardianCount] = useState(0)
+  const [premiumCount, setPremiumCount] = useState(0)
+  const [visitorCount, setVisitorCount] = useState(0)
   const [adminCount, setAdminCount] = useState(0)
   const [baoCount, setBaoCount] = useState(0)
   const [guardCount, setGuardCount] = useState(0)
   const [violationCount, setViolationCount] = useState(0)
   const [rfidCount, setRfidCount] = useState(0)
 
-  const fetchUsers = () => {
+  const fetchStudents = () => {
     axios
-      .get('/api/user/count')
+      .get('/api/user/count/student')
       .then(response => {
-        setUserCount(response.data)
+        setStudentCount(response.data)
+      })
+      .catch(error => console.error('Error fetching data', error))
+  }
+
+  const fetchStaffs = () => {
+    axios
+      .get('/api/user/count/staff')
+      .then(response => {
+        setStaffCount(response.data)
+      })
+      .catch(error => console.error('Error fetching data', error))
+  }
+
+  const fetchGuardians = () => {
+    axios
+      .get('/api/guardian/count')
+      .then(response => {
+        setGuardianCount(response.data)
+      })
+      .catch(error => console.error('Error fetching data', error))
+  }
+
+  const fetchPremiums = () => {
+    axios
+      .get('/api/premium/count')
+      .then(response => {
+        setPremiumCount(response.data)
+      })
+      .catch(error => console.error('Error fetching data', error))
+  }
+
+  const fetchVisitors = () => {
+    axios
+      .get('/api/visitor/count')
+      .then(response => {
+        setVisitorCount(response.data)
       })
       .catch(error => console.error('Error fetching data', error))
   }
@@ -54,7 +94,7 @@ const Home = () => {
       .catch(error => console.error('Error fetching data', error))
   }
 
-  const fetchViolations = () => {
+  /* const fetchViolations = () => {
     axios
       .get('/api/user/violations/count')
       .then(response => {
@@ -70,38 +110,71 @@ const Home = () => {
         setRfidCount(response.data)
       })
       .catch(error => console.error('Error fetching data', error))
-  }
+  } */
 
   // Fetch data on component mount
   useEffect(() => {
-    fetchUsers()
+    fetchStudents()
+    fetchStaffs()
+    fetchGuardians()
+    fetchPremiums()
+    fetchVisitors()
     fetchAdmins()
     fetchBAOs()
     fetchGuards()
-    fetchViolations()
-    fetchRFID()
+
+    /* fetchViolations()
+    fetchRFID() */
   }, [])
 
   return (
     <Grid container spacing={3}>
-      <Grid item xs={3} md={3}>
-        <UserDetails icon='mdi:account-group-outline' color='primary' count={userCount} title='Total Users' />
+      <Grid item xs={12} sm={12}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={4}>
+            <UserDetails icon='mdi:account-eye' color='primary' count={adminCount} title='Total Admins' />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <UserDetails icon='mdi:account-cash' color='primary' count={baoCount} title='Total BAOs' />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <UserDetails icon='mdi:account-tie-hat' color='primary' count={guardCount} title='Total Security Guards' />
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid item xs={3} md={3}>
-        <UserDetails icon='mdi:account-eye' color='primary' count={adminCount} title='Total Admins' />
+      <Grid item xs={12} sm={12}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={2}>
+            <UserDetails icon='mdi:account-group-outline' color='primary' count={studentCount} title='Total Users' />
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <UserDetails icon='mdi:account-group-outline' color='primary' count={staffCount} title='Total Staffs' />
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <UserDetails
+              icon='mdi:account-group-outline'
+              color='primary'
+              count={guardianCount}
+              title='Total Guardians'
+            />
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <UserDetails icon='mdi:account-group-outline' color='primary' count={premiumCount} title='Total Premiums' />
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <UserDetails icon='mdi:account-group-outline' color='primary' count={visitorCount} title='Total Visitors' />
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <UserDetails icon='mdi:car-back' color='primary' count={visitorCount} title='Total Vehicles' />
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid item xs={3} md={3}>
-        <UserDetails icon='mdi:account-cash' color='primary' count={baoCount} title='Total BAOs' />
-      </Grid>
-      <Grid item xs={3} md={3}>
-        <UserDetails icon='mdi:account-tie-hat' color='primary' count={guardCount} title='Total Security Guards' />
-      </Grid>
-      <Grid item xs={3} md={3}>
+      {/* <Grid item xs={12} md={3}>
         <UserDetails icon='mdi:alert-outline' color='warning' count={violationCount} title='Total Violations' />
       </Grid>
-      <Grid item xs={3} md={3}>
+      <Grid item xs={12} md={3}>
         <UserDetails icon='mdi:card-multiple-outline' color='success' count={rfidCount} title='Total RFID Scanned' />
-      </Grid>
+      </Grid> */}
     </Grid>
   )
 }
