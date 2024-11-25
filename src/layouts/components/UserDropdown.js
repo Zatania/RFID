1876965c6e7d7c9f591b2aa72be0e9d67c7479aa
John Binze Escol <escoljohnbinze@gmail.com>
2024-11-25 21:56:09 +0800

    Added styles.

--- src/layouts/components/UserDropdown.js
+++ src/layouts/components/UserDropdown.js
@@ -32,6 +32,21 @@ const BadgeContentSpan = styled('span')(({ theme }) => ({
   boxShadow: `0 0 0 2px ${theme.palette.background.paper}`
 }))
 
+const styles = {
+  py: 2,
+  px: 4,
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  color: 'text.primary',
+  textDecoration: 'none',
+  '& svg': {
+    mr: 2,
+    fontSize: '1.375rem',
+    color: 'text.primary'
+  }
+}
+
 const UserDropdown = props => {
   // ** Props
   const { settings } = props
