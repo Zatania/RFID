356468dd86d22f7068f1ac9a722c73f4da8a488a
John Binze Escol <escoljohnbinze@gmail.com>
2024-12-03 00:46:19 +0800

    Fixed query.

--- src/pages/api/bao/inactive/index.js
+++ src/pages/api/bao/inactive/index.js
@@ -4,22 +4,31 @@ const fetchInactives = async () => {
   try {
     const [result] = await db.query(`
         SELECT
-            COALESCE(users.id, premiums.id) AS id,
-            COALESCE(users.first_name, premiums.first_name) AS first_name,
-            COALESCE(users.middle_name, premiums.middle_name) AS middle_name,
-            COALESCE(users.last_name, premiums.last_name) AS last_name,
-            COALESCE(users.status, premiums.status) AS status,
-            rfids.load_balance AS load_balance,
-            CASE
-              WHEN users.id IS NOT NULL THEN 'User'
-              WHEN premiums.id IS NOT NULL THEN 'Premium'
-              ELSE 'Unknown'
-            END AS account_type
+            users.id,
+            users.first_name,
+            users.middle_name,
+            users.last_name,
+            users.status,
+            rfids.load_balance,
+            'User' AS account_type
         FROM
-            RFIDs rfids
-        LEFT JOIN Users users ON rfids.user_id = users.id
-        LEFT JOIN Premiums premiums ON rfids.premium_id = premiums.id
-        WHERE COALESCE(users.status, premiums.status) = 'Inactive';
+            users
+        LEFT JOIN RFIDs rfids ON rfids.user_id = users.id
+        WHERE
+            users.status = 'Inactive'
+        UNION ALL
+        SELECT
+            premiums.id,
+            premiums.first_name,
+            premiums.middle_name,
+            premiums.last_name,
+            premiums.status,
+            rfids.load_balance,
+            'Premium' AS account_type
+        FROM
+            premiums
+        LEFT JOIN RFIDs rfids ON rfids.premium_id = premiums.id
+        WHERE premiums.status = 'Inactive';
       `)
 
     return result
