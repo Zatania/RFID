265de447ed926f256e57c9c5edec0e51548f8599
John Binze Escol <escoljohnbinze@gmail.com>
2024-12-22 16:07:08 +0800

    Fetch bao added in api.

--- src/pages/api/bao/[bao_id].js
+++ src/pages/api/bao/[bao_id].js
@@ -11,6 +11,31 @@ const deleteBAO = async bao_id => {
   }
 }
 
+const fetchBAOInfo = async bao_id => {
+  try {
+    const [bao] = await db.query(
+      `
+      SELECT
+        id,
+        last_name,
+        first_name,
+        middle_name,
+        image,
+        username
+      FROM
+        baos
+      WHERE id = ?
+      `,
+      [bao_id]
+    )
+
+    return bao
+  } catch (error) {
+    console.error('SQL Error:', error)
+    throw error
+  }
+}
+
 const handler = async (req, res) => {
   try {
     if (req.method === 'DELETE') {
@@ -21,6 +46,24 @@ const handler = async (req, res) => {
         if (!bao) return res.status(400).json({ message: 'Failed to delete bao' })
 
         return res.status(200).json({ message: 'BAO deleted successfully' })
+      } catch (error) {
+        console.log(error)
+
+        if (error) {
+          return res.status(500).json({ message: error.message })
+        } else {
+          return res.status(500).json({ message: 'Something went wrong' })
+        }
+      }
+    }
+    if (req.method === 'GET') {
+      const { bao_id } = req.query
+      try {
+        const bao = await fetchBAOInfo(bao_id)
+
+        if (!bao) return res.status(400).json({ message: 'Failed to fetch bao information' })
+
+        return res.status(200).json({ bao })
       } catch (error) {
         console.log(error)
 
