29d8f4265eeef7c1925c9a7aaded95a2d0c2233b
John Binze Escol <escoljohnbinze@gmail.com>
2025-01-05 03:22:08 +0800

    Added fetching of guard info.

--- src/pages/api/guard/[guard_id].js
+++ src/pages/api/guard/[guard_id].js
@@ -11,6 +11,31 @@ const deleteGuard = async guard_id => {
   }
 }
 
+const fetchGuardInfo = async guard_id => {
+  try {
+    const [guard] = await db.query(
+      `
+      SELECT
+        id,
+        last_name,
+        first_name,
+        middle_name,
+        image,
+        username
+      FROM
+        security_guards
+      WHERE id = ?
+      `,
+      [guard_id]
+    )
+
+    return guard
+  } catch (error) {
+    console.error('SQL Error:', error)
+    throw error
+  }
+}
+
 const handler = async (req, res) => {
   try {
     if (req.method === 'DELETE') {
@@ -21,6 +46,23 @@ const handler = async (req, res) => {
         if (!guard) return res.status(400).json({ message: 'Failed to delete security guard' })
 
         return res.status(200).json({ message: 'Security Guard deleted successfully' })
+      } catch (error) {
+        console.log(error)
+
+        if (error) {
+          return res.status(500).json({ message: error.message })
+        } else {
+          return res.status(500).json({ message: 'Something went wrong' })
+        }
+      }
+    } else if (req.method === 'GET') {
+      const { guard_id } = req.query
+      try {
+        const guard = await fetchGuardInfo(guard_id)
+
+        if (!guard) return res.status(400).json({ message: 'Failed to fetch guard information' })
+
+        return res.status(200).json(guard[0])
       } catch (error) {
         console.log(error)
 
