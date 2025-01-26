dc76f5e487052a80f64d9507570b3ceed78819a7
John Binze Escol <escoljohnbinze@gmail.com>
2025-01-27 03:20:08 +0800

    Try using server ip instead.

--- src/views/pages/user/management/DialogAddUser.js
+++ src/views/pages/user/management/DialogAddUser.js
@@ -34,6 +34,7 @@ import { useForm, Controller } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import axios from 'axios'
 import * as bcrypt from 'bcryptjs'
+import dotenv from 'dotenv'
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Fade ref={ref} {...props} />
@@ -71,7 +72,7 @@ const DialogAddUser = ({ refreshData }) => {
 
   useEffect(() => {
     if (show && rfidRef.current) {
-      const socket = new WebSocket('ws://localhost:4000/user')
+      const socket = new WebSocket(`ws://${process.env.SERVER_IP}:4000/user`)
 
       console.log('Connecting to websocket server in adding user...')
 
